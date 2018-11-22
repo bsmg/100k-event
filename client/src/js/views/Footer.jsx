b9b7ec21034b41670babec4c328b60c822629afe
Jack Baron <jackmbaron@gmail.com>
2018-11-22 09:28:20 +0000

    image alt

--- client/src/js/views/Footer.jsx
+++ client/src/js/views/Footer.jsx
@@ -5,7 +5,7 @@ import BSMG from '../../images/bsmg.png'
 const Footer = () =>
   <div className='footer'>
     <span>Beat Saber Modding Group</span>
-    <img src={ BSMG } />
+    <img src={ BSMG } alt='BSMG Logo' />
     <span>100k Golden Ticket Raffle</span>
   </div>
 
